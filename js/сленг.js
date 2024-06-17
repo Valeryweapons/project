d149d2613cc3cd9b5e14e8d22304cfb696d995ba
Valeryweapons <117982209+Valeryweapons@users.noreply.github.com>
2024-06-17 15:48:51 +0300

    initial commit

---
+++ js/сленг.js
@@ -0,0 +1,51 @@
+"use strict"
+
+let isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+//Стрілка для меню
+let body = document.querySelector('body');
+if (isMobile.any()) {
+    body.classList.add('touch');
+    let arrow = document.querySelectorAll('.arrow');
+    for (let i = 0; i < arrow.length; i++) { // Оголошення змінної i за допомогою let
+        let thisLink = arrow[i].previousElementSibling;
+        let subMenu = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+        thisLink.classList.add('parent');
+        arrow[i].addEventListener('click', function() {
+            subMenu.classList.toggle('open');
+            thisArrow.classList.toggle('on');
+        });
+    }
+} else {
+    body.classList.add('mouse');
+}
+
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+if(iconMenu){
+    const menuBody = document.querySelector('.menu__body')
+    iconMenu.addEventListener('click', function(e){
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
